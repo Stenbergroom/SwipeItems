db9a2ce59e7189e9b305ea6a2af513cf0d557aa6
Stenbergroom <marrchenko@gmail.com>
2015-04-26 18:03:53 +0300

    Update ListViewAdapter.java

--- app/src/main/java/com/stenbergroom/swipeitems/app/adapter/ListViewAdapter.java
+++ app/src/main/java/com/stenbergroom/swipeitems/app/adapter/ListViewAdapter.java
@@ -2,8 +2,5 @@ package com.stenbergroom.swipeitems.app.adapter;
 
 import com.daimajia.swipe.adapters.BaseSwipeAdapter;
 
-/**
- * Created by Sten on 25.04.2015.
- */
 public class ListViewAdapter extends BaseSwipeAdapter {
 }
