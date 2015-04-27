158b7473285bc65a5904bf3fc6244eac11ca5638
Aleksandr Marchenko <marrchenko@gmail.com>
2015-04-27 22:20:26 +0300

    update files

--- app/src/main/java/com/stenbergroom/swipeitems/app/NestedExample.java
+++ app/src/main/java/com/stenbergroom/swipeitems/app/NestedExample.java
@@ -14,6 +14,7 @@ public class NestedExample extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.complicate_layout);
         SwipeLayout swipeLayout = (SwipeLayout) findViewById(R.id.test_swipe_swipe);
         swipeLayout.setOnDoubleClickListener(new SwipeLayout.DoubleClickListener() {
             @Override
