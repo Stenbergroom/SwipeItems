d38a7cd0525e2b7e69bbb65ddb9ac0b05e281534
Aleksandr Marchenko <marrchenko@gmail.com>
2015-04-27 17:38:46 +0300

    update ListViewAdapter class

--- app/src/main/java/com/stenbergroom/swipeitems/app/adapter/ListViewAdapter.java
+++ app/src/main/java/com/stenbergroom/swipeitems/app/adapter/ListViewAdapter.java
@@ -1,6 +1,87 @@
 package com.stenbergroom.swipeitems.app.adapter;
 
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+import com.daimajia.androidanimations.library.Techniques;
+import com.daimajia.androidanimations.library.YoYo;
+import com.daimajia.swipe.SwipeLayout;
 import com.daimajia.swipe.adapters.BaseSwipeAdapter;
+import com.stenbergroom.swipeitems.app.R;
 
 public class ListViewAdapter extends BaseSwipeAdapter {
+
+    private Context mContext;
+
+    public ListViewAdapter(Context mContext) {
+        this.mContext = mContext;
+    }
+
+    @Override
+    public int getSwipeLayoutResourceId(int position) {
+        return R.id.swipe;
+    }
+
+    @Override
+    public View generateView(int position, ViewGroup parent) {
+        View v = LayoutInflater.from(mContext).inflate(R.layout.listview_item, null);
+        SwipeLayout swipeLayout = (SwipeLayout) v.findViewById(getSwipeLayoutResourceId(position));
+        swipeLayout.addSwipeListener(new SwipeLayout.SwipeListener() {
+            @Override
+            public void onStartOpen(SwipeLayout swipeLayout) {
+            }
+
+            @Override
+            public void onOpen(SwipeLayout layout) {
+                YoYo.with(Techniques.Tada).duration(500).delay(100).playOn(layout.findViewById(R.id.trash));
+            }
+
+            @Override
+            public void onStartClose(SwipeLayout swipeLayout) {
+            }
+
+            @Override
+            public void onClose(SwipeLayout swipeLayout) {
+            }
+
+            @Override
+            public void onUpdate(SwipeLayout swipeLayout, int i, int i1) {
+            }
+
+            @Override
+            public void onHandRelease(SwipeLayout swipeLayout, float v, float v1) {
+            }
+        });
+        swipeLayout.setOnDoubleClickListener(new SwipeLayout.DoubleClickListener() {
+            @Override
+            public void onDoubleClick(SwipeLayout layout, boolean surface) {
+                Toast.makeText(mContext, "DoubleClick", Toast.LENGTH_SHORT).show();
+            }
+        });
+        return v;
+    }
+
+    @Override
+    public void fillValues(int position, View convertView) {
+        TextView t = (TextView) convertView.findViewById(R.id.position);
+        t.setText((position + 1) + ".");
+    }
+
+    @Override
+    public int getCount() {
+        return 50;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
 }
