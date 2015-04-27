34d036103635ce217d6879bafcda75c06b1e2ab9
Aleksandr Marchenko <marrchenko@gmail.com>
2015-04-27 17:49:22 +0300

    update GridViewExample class

--- app/src/main/java/com/stenbergroom/swipeitems/app/GridViewExample.java
+++ app/src/main/java/com/stenbergroom/swipeitems/app/GridViewExample.java
@@ -1,9 +1,53 @@
 package com.stenbergroom.swipeitems.app;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.Toast;
+import com.daimajia.swipe.implments.SwipeItemMangerImpl;
+import com.stenbergroom.swipeitems.app.adapter.GridViewAdapter;
 
 /**
  * Created by Sten on 25.04.2015.
  */
 public class GridViewExample extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.gridview);
+
+        final GridView gridView = (GridView)findViewById(R.id.gridview);
+        final GridViewAdapter adapter = new GridViewAdapter(this);
+
+        adapter.setMode(SwipeItemMangerImpl.Mode.Multiple);
+        gridView.setAdapter(adapter);
+        gridView.setSelected(false);
+        gridView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                Log.d("onItem", "onItemLongClick: " + position);
+                return false;
+            }
+        });
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Log.d("onItem", "onItemClick: " + position);
+            }
+        });
+        gridView.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Log.d("onItem", "onItemSelected: " + position);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+    }
 }
